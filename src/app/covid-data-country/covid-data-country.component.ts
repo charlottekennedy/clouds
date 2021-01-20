d4a333a39423cec72bc1764b9d8f570fb9d001c9
Charlotte <charlotte.kennedy@telecm-paris.fr>
2021-01-20 17:04:46 +0100

    bar and line charts without real data

--- src/app/covid-data-country/covid-data-country.component.ts
+++ src/app/covid-data-country/covid-data-country.component.ts
@@ -3,62 +3,75 @@ import { DataCountry } from '../dataCountry.model';
 import { CovidService } from '../covid.service';
 import { News } from '../news.model';
 import { ActivatedRoute } from '@angular/router';
-import { ChartType, ChartOptions } from 'chart.js';
+import { ChartType, ChartOptions, ChartDataSets } from 'chart.js';
 import { SingleDataSet, Label, monkeyPatchChartJsLegend, monkeyPatchChartJsTooltip } from 'ng2-charts';
 
 @Component({
-  selector: 'app-covid-data-country',
-  templateUrl: './covid-data-country.component.html',
-  styleUrls: ['./covid-data-country.component.css']
+    selector: 'app-covid-data-country',
+    templateUrl: './covid-data-country.component.html',
+    styleUrls: ['./covid-data-country.component.css']
 })
 export class CovidDataCountryComponent implements OnInit {
     news : News[];
-    private sub: any;
-    name : String;
-    dataC: DataCountry;
+private sub: any;
+name : String;
+dataC: DataCountry;
 
-  public pieChartOptions: ChartOptions = {
+public pieChartOptions: ChartOptions = {
     responsive: true,
-  };
-  public pieChartLabels: Label[] = ['Dead Cases', 'Recovered Cases', 'Active Cases'];
-  public pieChartData: SingleDataSet;
-  public pieChartType: ChartType = 'pie';
-  public pieChartLegend = true;
-  public pieChartPlugins = [];
+};
+public pieChartLabels: Label[] = ['Dead Cases', 'Recovered Cases', 'Active Cases'];
+public pieChartData: SingleDataSet;
+public pieChartType: ChartType = 'pie';
+public pieChartLegend = true;
+public pieChartPlugins = [];
 
 
-  constructor(private route: ActivatedRoute, public covidService : CovidService) { }
+barChartOptions: ChartOptions = {
+    responsive: true,
+};
+barChartLabels: Label[] = ['Apple', 'Banana', 'Kiwifruit', 'Blueberry', 'Orange', 'Grapes'];
+barChartType: ChartType = 'bar';
+barChartLegend = true;
+barChartPlugins = [];
+barChartData: ChartDataSets[];
 
-  ngOnInit(): void {
-      
-      this.sub = this.route.paramMap.subscribe(params => {
-      this.name = params.get('name');
-  })
-  
-  this.dataC = this.covidService.getDataOneCountry(this.name);
+
+lineChartData: ChartDataSets[]; 
+lineChartLabels: Label[] = ['January', 'February', 'March', 'April', 'May', 'June'];
+lineChartOptions = {
+    responsive: true,
+};
+
+lineChartLegend = true;
+lineChartPlugins = [];
+lineChartType = 'line';
+
+
+constructor(private route: ActivatedRoute, public covidService : CovidService) { }
+
+ngOnInit(): void {
+
+    this.sub = this.route.paramMap.subscribe(params => {
+    this.name = params.get('name');
+})
+
+this.dataC = this.covidService.getDataOneCountry(this.name);
 
 this.pieChartData =  [this.dataC.totalDeaths, this.dataC.totalRecovered, this.dataC.activeCases];
-/*this.pieChart();*/
-  }
-  
-  /*pieChart(){
-      var canva = document.getElementById<HTMLCanvasElement>("pieChart");
-  	  var ctxP = canva.getContext('2d');
-      var myPieChart = new Chart(ctxP, {
-      type: 'pie',
-      data: {
-        labels: ["Dead Cases", "Recovered Cases", "Active Cases"],
-        datasets: [{
-          data: [this.dataC.totalDeaths, this.dataC.totalRecovered, this.dataC.activeCases],
-          backgroundColor: ["#F7464A", "#46BFBD", "#FDB45C"],
-          hoverBackgroundColor: ["#FF5A5E", "#5AD3D1", "#FFC870"]
-        }]
-      },
-      options: {
-        responsive: true
-      }
-    });
-      
-}*/
+
+this.barChartData= [
+    { data: [45, 37, 60, 70, 46, 33], label: 'Daily Deaths' }, { data: [45, 37, 60, 70, 46, 33], label: 'Daily Recovered' },{ data: [45, 37, 60, 70, 46, 33], label: 'Daily New Cases' }
+];
+
+this.lineChartData = [
+    { data: [85, 72, 78, 75, 77, 75], label: 'Total Deaths' },
+    { data: [85, 32, 78, 78, 77, 15], label: 'Total Recovered' },
+    { data: [85, 72, 32, 40, 77, 75], label: 'Total Cases' },
+];
+
 }
-    
+
+
+}
+
--- src/app/covid-data/covid-data.component.ts
+++ src/app/covid-data/covid-data.component.ts
@@ -5,61 +5,86 @@ import { User } from '../user.model';
 import { News } from '../news.model';
 import { DataCountry } from '../dataCountry.model';
 import { DataWorld } from '../dataWorld.model';
-import { ChartType, ChartOptions } from 'chart.js';
 import { Sort } from '@angular/material/sort';
+import { ChartType, ChartOptions, ChartDataSets } from 'chart.js';
 import { SingleDataSet, Label, monkeyPatchChartJsLegend, monkeyPatchChartJsTooltip } from 'ng2-charts';
 
 @Component({
-  selector: 'app-covid-data',
-  templateUrl: './covid-data.component.html',
-  styleUrls: ['./covid-data.component.css']
+    selector: 'app-covid-data',
+    templateUrl: './covid-data.component.html',
+    styleUrls: ['./covid-data.component.css']
 })
 
 
 export class CovidDataComponent implements OnInit {
 
-  news: News[];
-  user: User;
-  dataCountry: Array<DataCountry>;
-  dataWorld : DataWorld;
-  sortedData: Array<DataCountry>;
+    news: News[];
+user: User;
+dataCountry: Array<DataCountry>;
+dataWorld : DataWorld;
+sortedData: Array<DataCountry>;
 
 
-  public pieChartOptions: ChartOptions = {
+public pieChartOptions: ChartOptions = {
     responsive: true,
-  };
-  public pieChartLabels: Label[] = ['Dead Cases', 'Recovered Cases', 'Active Cases'];
-  public pieChartData: SingleDataSet;
-  public pieChartType: ChartType = 'pie';
-  public pieChartLegend = true;
-  public pieChartPlugins = [];
-  
-  constructor(public covidService : CovidService) { 
-      monkeyPatchChartJsTooltip();
-    monkeyPatchChartJsLegend();
-  }
+};
+public pieChartLabels: Label[] = ['Dead Cases', 'Recovered Cases', 'Active Cases'];
+public pieChartData: SingleDataSet;
+public pieChartType: ChartType = 'pie';
+public pieChartLegend = true;
+public pieChartPlugins = [];
 
-  ngOnInit(): void {
-  	/*this.covidService.displayNews("worldwide").subscribe((news: News[])=>{
+barChartOptions: ChartOptions = {
+    responsive: true,
+};
+barChartLabels: Label[] = ['Apple', 'Banana', 'Kiwifruit', 'Blueberry', 'Orange', 'Grapes'];
+barChartType: ChartType = 'bar';
+barChartLegend = true;
+barChartPlugins = [];
+barChartData: ChartDataSets[];
+
+
+lineChartData: ChartDataSets[]; 
+lineChartLabels: Label[] = ['January', 'February', 'March', 'April', 'May', 'June'];
+lineChartOptions = {
+    responsive: true,
+};
+lineChartLegend = true;
+lineChartPlugins = [];
+lineChartType = 'line';
+
+constructor(public covidService : CovidService) { 
+    monkeyPatchChartJsTooltip();
+    monkeyPatchChartJsLegend();
+}
+
+ngOnInit(): void {
+    /*this.covidService.displayNews("worldwide").subscribe((news: News[])=>{
   		this.news = news;
   	}); */
-  	this.user = this.covidService.getUser();
-  	/*this.covidService.displayNews().subscribe((news: News[])=>{
+    this.user = this.covidService.getUser();
+    /*this.covidService.displayNews().subscribe((news: News[])=>{
   		this.news = news;
   	});*/
-  	
-  	this.dataWorld = this.covidService.getDataWorld();
+
+    this.dataWorld = this.covidService.getDataWorld();
     this.dataCountry = this.covidService.getDataCountry();
     this.sortedData = this.dataCountry;
-    console.log(this.dataWorld.totalDeaths);
+
     this.pieChartData =  [this.dataWorld.totalDeaths, this.dataWorld.totalRecovered, this.dataWorld.activeCases];
 
-  	/*this.covidService.getDataCountries().subscribe(
-  	  dataCountry => this.dataCountry = dataCountry
-  	);*/
-  	
-  }
-  
+    this.barChartData= [
+    { data: [45, 37, 60, 70, 46, 33], label: 'Daily Deaths' }, { data: [45, 37, 60, 70, 46, 33], label: 'Daily Recovered' },{ data: [45, 37, 60, 70, 46, 33], label: 'Daily New Cases' }
+];
+
+this.lineChartData = [
+    { data: [85, 72, 78, 75, 77, 75], label: 'Total Deaths' },
+    { data: [85, 32, 78, 78, 77, 15], label: 'Total Recovered' },
+    { data: [85, 72, 32, 40, 77, 75], label: 'Total Cases' },
+];
+
+}
+
 
 sortData(sort: Sort){
     this.sortedData = this.dataCountry.slice();
@@ -68,7 +93,7 @@ sortData(sort: Sort){
         this.sortedData = data;
         return;
     }
-    
+
     this.sortedData = data.sort((a,b) => {
         const isAsc = sort.direction === 'asc';
         switch (sort.active){
